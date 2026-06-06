11c3e49263b659ab550868d60fcc033735415bff
deepdivesecurity <165413290+deepdivesecurity@users.noreply.github.com>
2026-06-06 18:42:18 -0400

    Removing express x-powered-by indicator

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+app.disable('x-powered-by');
 app.use(express.static(path.join(__dirname, "public")));
 
 const expressServer = app.listen(PORT, () => {
